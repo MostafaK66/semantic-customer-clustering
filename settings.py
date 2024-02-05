7cf87eb7f357f40896eb234a887448b4e3cfd6d5
Mostafa Karimzadeh <mos.karimzadeh@gmail.com>
2024-02-05 11:06:37 +0100

    Minor cleaning on settings.py.

--- settings.py
+++ settings.py
@@ -11,14 +11,3 @@ PCA_N_ITER = 3
 TSNE_LEARNING_RATE = 500
 TSNE_PERPLEXITY = 200
 TSNE_N_ITER = 5000
-
-
-"""
-
-numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
-
-categorical_columns = df_no_outliers_norm.select_dtypes(exclude=numerics).columns
-print(categorical_columns)
-categorical_columns_index = [df_no_outliers_norm.columns.get_loc(col) for col in categorical_columns]
-
-"""
