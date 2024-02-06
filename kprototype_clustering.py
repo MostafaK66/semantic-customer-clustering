d1ba2b273d1227fa668ab4269dcb9aa2a54102f8
Mostafa Karimzadeh <mos.karimzadeh@gmail.com>
2024-02-06 11:40:41 +0100

    Added progress bar to find_optimal_clusters method.

--- kprototype_clustering.py
+++ kprototype_clustering.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from kmodes.kprototypes import KPrototypes
 import settings
+from tqdm import tqdm
 
 
 class KPrototypeClustering:
@@ -13,11 +14,10 @@ class KPrototypeClustering:
 
         df_sampled = df_no_outliers.sample(frac=0.1, replace=True, random_state=self.random_state)
 
-        for cluster in range_:
+        for cluster in tqdm(range_, desc="Running KPrototype Clustering"):
             kprototype = KPrototypes(n_jobs=-1, n_clusters=cluster, init='Huang', random_state=self.random_state)
             kprototype.fit_predict(df_sampled, categorical=categorical_columns_index)
             cost.append(kprototype.cost_)
-            print(f'Cluster initiation: {cluster}')
 
         return pd.DataFrame({'Cluster': range_, 'Cost': cost})
 
--- utils.py
+++ utils.py
@@ -62,7 +62,6 @@ class DataPreprocessor:
         numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
 
         categorical_columns = df.select_dtypes(exclude=numerics).columns
-        print(categorical_columns)
 
         categorical_columns_index = [df.columns.get_loc(col) for col in categorical_columns]
 
